908a56bbfdb5517d8b7051740b474bf931f7b00f
Gabe-max-lab-cloud <gabrielbognot@riccarton.school.nz>
2025-04-10 12:52:55 +1200

    Create monster_card_assessment.py

---
+++ monster_card_assessment.py
@@ -0,0 +1,83 @@
+import easygui.msgbox
+
+
+Cards = input{
+    "Stoneling": {
+    'strenght': 7,
+    'speed': 1,
+    'stealth': 25,
+    'cunning': 15,
+    }
+
+    "Vexscream": {
+    'strenght': 1,
+    'speed': 6,
+    'stealth': 21,
+    'cunning': 19,
+    }
+
+    "Dawnmirage": {
+    'strenght': 5,
+    'speed': 15,
+    'stealth': 18,
+    'cunning': 22,
+    }
+
+    "Blazegolem": {
+    'strenght': 15,
+    'speed': 20,
+    'stealth': 23,
+    'cunning': 6,
+    }
+
+    "Websnake": {
+    'strenght': 7,
+    'speed': 15,
+    'stealth': 10,
+    'cunning': 5,
+    }
+
+    "Moldvine": {
+    'strenght': 21,
+    'speed': 18,
+    'stealth': 14,
+    'cunning': 5,
+    }
+
+    "Vortexwing": {
+    'strenght': 19,
+    'speed': 13,
+    'stealth': 19,
+    'cunning': 2,
+    }
+
+    "Rotthing": {
+    'strenght': 16,
+    'speed': 7,
+    'stealth': 4,
+    'cunning': 12,
+    }
+
+    "Froststep": {
+    'strenght': 14,
+    'speed': 14,
+    'stealth': 17,
+    'cunning': 4,
+    }
+
+    "Wispghoul": {
+    'strenght': 17,
+    'speed': 19,
+    'stealth': 3,
+    'cunning': 2,
+    }
+
+}
+
+
+
+
+def display_cards(cards):
+    message=
+    message=
+    message=
